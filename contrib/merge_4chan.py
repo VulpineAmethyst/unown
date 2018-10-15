1708d03274c34202de830fe70c6bc0c82703cb6f
Kiyoshi Aman <kiyoshi.aman@gmail.com>
2018-10-15 01:47:29 -0500

    Unown is an AGPL-3.0 project.

--- contrib/merge_4chan.py
+++ contrib/merge_4chan.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+#
+# merge_4chan.py merges posts extracted by extract_4chan.py in to a
+# single document.
+# Copyright (C) 2018 Kiyoshi Aman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program. If not, see
+# <https://www.gnu.org/licenses/>.
 
 import os
 import os.path as op
